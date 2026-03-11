2e04b5e823f7f762e376cc462f2de352e11e55e3
d4nielxo <dhaddad3@students.avc.edu>
2026-03-11 11:46:17 -0700

    program 2 # Please enter the commit message for your changes. Lines starting

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-int main()
-{
-  int numFemale, numMale, numOthers;
-  double percF, percM, percO;
 
-  cout << "Enter the number of students: Male, Female and Others";
-  cin >> numMale >> numFemale >> numOthers;
-  // TODO
+int main() {
+    int numFemale, numMale, numOthers;
+    double percF, percM, percO;
 
-  cout << setprecision(2) << fixed;
-  cout << "Percentage of Male: " << percM << endl;
-  cout << "Percentage of Female: " << percF << endl;
-  cout << "Percentage of Others: " << percO << endl;
+    cout << "Enter the number of students: Male, Female and Others";
+    cin >> numMale >> numFemale >> numOthers;
+
+    int total = numMale + numFemale + numOthers;
+
+    percM = (static_cast<double>(numMale) / total) * 100.0;
+    percF = (static_cast<double>(numFemale) / total) * 100.0;
+    percO = (static_cast<double>(numOthers) / total) * 100.0;
+
+    cout << setprecision(2) << fixed;
+    cout << "Percentage of Male: " << percM << endl;
+    cout << "Percentage of Female: " << percF << endl;
+    cout << "Percentage of Others: " << percO << endl;
+
+    return 0;
 }
